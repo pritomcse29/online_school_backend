4bdf4f196b95d560cbbbaef2f9451e6bbf5579c0
Pritom Sarkar <pritom.cse.29@gmail.com>
2025-07-31 16:55:03 +0600

    settings update

--- online_school/settings.py
+++ online_school/settings.py
@@ -71,7 +71,6 @@ CORS_ALLOWED_ORIGINS = [
   "http://127.0.0.1:8000",
   "http://localhost:5173",
   "https://online-school-backend-1.onrender.com",
-  "https://online-school-frontend-wbqk.vercel.app/",
    "https://online-school-frontend-wbqk.vercel.app",
 
 ]
